accbdc59d9d58295424e2b2e6ce8857f9b24d55a
Conrad008 <kipngeno7180@gmail.com>
2026-07-15 00:40:09 +0300

    feat:add Google Sign-In handler and button UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import Landing from "./pages/Landing"
+// import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-      
+    {/* <Navbar/> */}
+    <Landing />
     </>
   )
 }
