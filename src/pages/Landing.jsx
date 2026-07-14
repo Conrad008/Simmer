22b8b2bbeb687627b17b87a35c29dad033e8ec8a
Conrad008 <kipngeno7180@gmail.com>
2026-07-14 13:58:23 +0300

    feat:import necessary components from ui library

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import { ChefHat, Sparkles } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function Landing() {
+    return (
+        
+    )
+}
