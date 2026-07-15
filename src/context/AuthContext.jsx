76745e75d783c7f91c7f0f4b6b84cf3885e85d2f
Conrad008 <kipngeno7180@gmail.com>
2026-07-15 11:22:46 +0300

    feat: create AuthProvider component and email login function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,28 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged, updateProfile } from "firebase/auth";
-import { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, signOut, GoogleAuthProvider,} from "@/config/firebase";
+import { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, signOut, GoogleAuthProvider, } from "@/config/firebase";
 
 const AuthContext = createContext(undefined);
- 
+
 const googleProvider = new GoogleAuthProvider();
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [authLoading, setAuthLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+            setUser(firebaseUser);
+            setAuthLoading(false);
+        });
+
+        return unsubscribe;
+    }, []);
+
+    const login = async (email, password) => {
+        const result = await signInWithEmailAndPassword(auth, email, password);
+        return result.user;
+    };
+
+
+}
