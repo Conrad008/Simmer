5f87adab2d7322dba20d85c9d3fdcdb42572d8dc
Conrad008 <kipngeno7180@gmail.com>
2026-07-15 12:02:41 +0300

    feat: export AuthProvider and implement useAuth custom hook

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -51,6 +51,14 @@ export function AuthProvider({ children }) {
         loginWithGoogle,
         logout,
     };
-    
 
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
 }
