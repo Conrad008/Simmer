559b30a1b665b6ede41691fd4c1c3c75c930458e
Conrad008 <kipngeno7180@gmail.com>
2026-07-18 13:38:05 +0300

    feat: add routes for Home, Favorites, My Recipes, and Recipe Detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Navbar from "./components/Navbar"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Recipes from "./pages/Recipes"
+import Home from "./pages/Home"
+import FavouritePage from "./pages/FavouritePage"
+import MyRecipes from "./pages/Myrecipes"
+import RecipeDetail from "./pages/RecipeDetail"
 
 function App() {
 
@@ -13,9 +17,13 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/favorites" element={<FavouritePage />} />
+        <Route path="/my-recipes" element={<MyRecipes />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
     </>
